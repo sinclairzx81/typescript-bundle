b0da6651890ab6c97c3f953b7934139f4a4a446e
sinclairzx81 <haydn.developer@gmail.com>
2017-07-17 19:05:13 +1200

    remove bin

--- bin/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var options_1 = require("./options");
-var bundle_1 = require("./bundle");
-var log = function (data) { return process.stdout.write(data); };
-var main = function (args) { return bundle_1.bundle(options_1.parse(args), log); };
-main(process.argv).catch(function (error) { return log(error); });
